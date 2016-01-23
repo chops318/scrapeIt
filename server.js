42826d1446bcf2bcf91aa8640a048ce688a5850a
Edward Smith <sspohC@gmail.com>
2016-01-23 00:10:38 -0800

    Selected first property

--- server.js
+++ server.js
@@ -4,22 +4,20 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res) {
-  url = 'http://sonlite.dnr.state.la.us/sundown/cart_prod/cart_con_wellinfo2?p_wsn=233'
+url = 'http://sonlite.dnr.state.la.us/sundown/cart_prod/cart_con_wellinfo2?p_wsn=233'
 
-      request(url, function(error, response, html) {
-        if (!error) {
-          var $ = cheerio.load(html);
-          //  console.log($)
-          var wellName;
-          var json = {wellName: " "}
-          wellName = $('table tbody td')[0];
-          console.log(wellName);
+request(url, function(error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html);
+                //  console.log($)
+                var wellName;
+                var json = {
+                    wellName: " "
+                }
+                wellName = $('table td').first().text();
+                console.log(wellName);
 
-      }})
-
-});
-
+            }});
 app.listen('9001');
 console.log("the magic happens on port 9001")
 
