80f8f098d1cd8f1a4a3e07cf08afa57c8d730626
Edward Smith <sspohC@gmail.com>
2016-01-24 15:05:55 -0800

    Pushing to github

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var async = require('async')
+var async = require('async');
+var json2xls = require('json2xls');
 var app     = express();
 
 var wellAray = [];
+
+
 
 
 var q = async.queue(function(task, done) {
@@ -20,24 +23,32 @@ var q = async.queue(function(task, done) {
     json.wellName = $('table td').eq(1).text();
     json.orgId = $('table td').eq(3).text();
     wellAray.push(json);
-    console.log(wellAray)
-    done();
+    console.log(wellArray);
 
     fs.writeFile('output.json', JSON.stringify(wellAray, null, 4), function(err) {
 
     })
+    done();
   })
-}, 5)
+}, 40);
 
 
-
-for (var i = 233; i < 243; i++){
+for (var i = 233; i < 633; i++){
   q.push({url: 'http://sonlite.dnr.state.la.us/sundown/cart_prod/cart_con_wellinfo2?p_wsn=' + i})
 }
 
+app.use(json2xls.middleware);
 
-app.get('/scrape', function() {
 
+app.get('/wells', function() {
+  fs.readFile('output.json', 'utf-8', function(err, data) {
+    if (err) throw err;
+    json = JSON.parse(data);
+    console.log(json)
+    wellAray = json2xls(json);
+    fs.writeFileSync('data.xlsx', wellAray, 'binary')
+
+  })
 })
 
 app.listen('9001');
