01fd7d49f598d31df279ffb88ea36385b1b76586
Edward Smith <sspohC@gmail.com>
2016-01-23 16:18:25 -0800

    Adding json2xls

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 var async = require('async')
 var app     = express();
 
-    var wellAray = [];
+var wellAray = [];
 
 
 var q = async.queue(function(task, done) {
@@ -13,6 +13,8 @@ var q = async.queue(function(task, done) {
     if (err) return done(err);
     var $ = cheerio.load(html);
     var json = {};
+    if ($('table td').eq(2).text() === "") return;
+
     json.serialNum = $('table td').eq(2).text();
     json.wellNum = $('table td').eq(0).text();
     json.wellName = $('table td').eq(1).text();
@@ -21,16 +23,21 @@ var q = async.queue(function(task, done) {
     console.log(wellAray)
     done();
 
-    fs.writeFile('output.json', JSON.stringify(wellAray, null, 2), function(err) {
+    fs.writeFile('output.json', JSON.stringify(wellAray, null, 4), function(err) {
 
     })
   })
 }, 5)
 
 
-for (var i = 233; i < 236; i++){
+
+for (var i = 233; i < 243; i++){
   q.push({url: 'http://sonlite.dnr.state.la.us/sundown/cart_prod/cart_con_wellinfo2?p_wsn=' + i})
 }
 
+
+app.get('/scrape', function() {
+
+})
 
 app.listen('9001');
